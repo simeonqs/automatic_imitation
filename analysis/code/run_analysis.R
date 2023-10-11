2486b75783086e5c874676b5e1a53a5d8a5fb93a
Simeon Q. Smeele <simeonqs@hotmail.com>
2023-10-11 11:49:59 +0200

    Updating run_analysis.R. Finishing up the first version. Both models work now.

--- analysis/code/run_analysis.R
+++ analysis/code/run_analysis.R
@@ -1,11 +1,11 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: automatic imitation  
 # Author: Simeon Q. Smeele
-# Description: 
+# Description: Runs the analysis.
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
-libraries = c('cmdstanr', 'readxl')
+libraries = c('cmdstanr', 'rethinking')
 for(lib in libraries){
   if(! lib %in% installed.packages()) lapply(lib, install.packages)
   lapply(libraries, require, character.only = TRUE)
@@ -15,8 +15,58 @@ for(lib in libraries){
 rm(list=ls()) 
 
 # Paths
-path_data = 'analysis/data/Dummy-data.xlsx'
+path_data = 'analysis/data/dummy_data.csv'
+path_model_trials = 'analysis/code/model_trials.stan'
+path_model_latency = 'analysis/code/model_latency.stan'
+path_results_trials = 'analysis/results/results_trials.RData'
+path_results_latency = 'analysis/results/results_latency.RData'
 
 # Read data
-dat = read_xlsx(path_data)
+dat = read.csv2(path_data)
 
+# Prepare data for trial model
+clean_dat = list(N_obs = nrow(dat),
+                 N_ind = length(unique(dat$Individual)),
+                 N_beh = length(unique(dat$Behaviours)),
+                 group = as.integer(as.factor(dat$Group)),
+                 ind = as.integer(as.factor(dat$Individual)),
+                 beh = as.integer(as.factor(dat$Behaviours)),
+                 response = as.integer(dat$Response))
+
+# Run trial model
+model = cmdstan_model(path_model_trials)
+fit = model$sample(data = clean_dat, 
+                   seed = 1, 
+                   chains = 4, 
+                   parallel_chains = 4,
+                   refresh = 500) 
+fit_nice = fit$output_files() |>
+  rstan::read_stan_csv()
+print(precis(fit_nice))
+
+# Save output latency
+save(fit_nice, file = path_results_latency)
+
+# Prepare data for latency model
+clean_dat = list(N_obs = nrow(dat),
+                 N_ind = length(unique(dat$Individual)),
+                 N_beh = length(unique(dat$Behaviours)),
+                 group = as.integer(as.factor(dat$Group)),
+                 ind = as.integer(as.factor(dat$Individual)),
+                 beh = as.integer(as.factor(dat$Behaviours)),
+                 latency = log(dat$Latency))
+
+# Run latency model
+model = cmdstan_model(path_model_latency)
+fit = model$sample(data = clean_dat, 
+                   seed = 1, 
+                   chains = 4, 
+                   parallel_chains = 4,
+                   refresh = 500,
+                   adapt_delta = 0.99) 
+fit_nice = fit$output_files() |>
+  rstan::read_stan_csv()
+print(precis(fit_nice))
+
+# Save output latency
+save(fit_nice, file = path_results_latency)
